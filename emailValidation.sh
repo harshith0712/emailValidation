bf78140b52ac5e965a2cbe01ea73c36bb1c3945c
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-13 19:36:04 +0530

    master

--- emailValidation.sh
+++ emailValidation.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome to Email Validation Problem"
 
+echo "enter the email"
+read email
+emailPattern="^[a-zA-Z0-9_.+-]*+@[a-zA-Z0-9]*+.[a-zA-Z]{2,4}|.[a-zA-Z]{2}$"
+
+if [[ $email =~ $emailPattern ]]
+then
+echo "valid"
+else
+echo "invalid"
+fi
+
